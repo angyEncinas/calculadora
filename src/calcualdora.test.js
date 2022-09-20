48b987bc2b1a2b0d7371237cc64dc9a754b11bb2
Angy Encinas <angy.encinas@ucb.edu.bo>
2022-09-19 20:10:06 -0400

    5: Tomar en cuenta mas de dos caracteres y ambos delimitadores

--- src/calcualdora.test.js
+++ src/calcualdora.test.js
@@ -13,4 +13,7 @@ describe("Sumar", () => {
   it("deberia devolver una suma de dos numeros dado el 2do delimitador", () => {
     expect(sumar("7-1")).toEqual(8);
   });
+  it("deberia devolver una suma de varios numeros con ambos delimitadores", () => {
+    expect(sumar("7-1,6")).toEqual(14);
+  });
 });
--- src/calculadora.js
+++ src/calculadora.js
@@ -3,27 +3,17 @@ function sumar(cadena) {
   if(cadena == ""){
     return 0;
   }
-  if(cadena.includes(',')){
-    cadenaTrans = separarPorDelimitador(cadena, ',');
-    let suma =0;
+  cadenaTrans = separarPorDelimitador(cadena);
+  let suma =0;
     for(var i=0;i<cadenaTrans.length;i++){
       suma = suma + Number(cadenaTrans[i]);
     }
-    return suma;
-  }
-  if(cadena.includes('-')){
-    cadenaTrans = separarPorDelimitador(cadena, '-');
-    let suma =0;
-    for(var i=0;i<cadenaTrans.length;i++){
-      suma = suma + Number(cadenaTrans[i]);
-    }
-    return suma;
-  }
-  return Number(cadena);
+  return suma;
 }
-function separarPorDelimitador(cadena,delimitador){
+function separarPorDelimitador(cadena){
+  var regx = new RegExp(',|-');
   let suma = []
-  suma = cadena.split(delimitador);
+  suma = cadena.split(regx);
   return suma;
 }
 export default sumar;
